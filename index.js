faa29244862991d575f98f4b719fb8cb542b1329
bladi452 <cleto662@gmail.com>
2021-10-13 14:22:26 -0400

    Prueba uno del update remoto

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const {autoUpdater} = require('electron-updater')
+
 const {createWindow} = require('./main.js')
 const {app} = require('electron')
 
 require('electron-reload')(__dirname);
+
+autoUpdater.checkForUpdatesAndNotify()
 
 app.allowRendererProcessReuse = false;
 app.whenReady().then(createWindow)
